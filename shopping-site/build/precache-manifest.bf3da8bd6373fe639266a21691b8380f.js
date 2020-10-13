f0558d5d98768030aa285eb69bebf38624028007
myron sin <myronbankbank@gmail.com>
2020-10-13 22:39:39 +0800

    build version

---
+++ shopping-site/build/precache-manifest.bf3da8bd6373fe639266a21691b8380f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564b26f84d253c672c872f57cec258a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3761174e556ac0b157c0",
+    "url": "/static/css/main.0ca8112f.chunk.css"
+  },
+  {
+    "revision": "d62a34b0658da22e1dc9",
+    "url": "/static/js/2.82d010cd.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.82d010cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3761174e556ac0b157c0",
+    "url": "/static/js/main.1307c79a.chunk.js"
+  },
+  {
+    "revision": "976bc2dd75b6c4282e2e",
+    "url": "/static/js/runtime-main.e0f5a793.js"
+  },
+  {
+    "revision": "06715f594d8baf23286fada7c93b5487",
+    "url": "/static/media/DSCF8515_edited.06715f59.jpg"
+  },
+  {
+    "revision": "7d24e19b75acffbfade8f81ed1563a1d",
+    "url": "/static/media/DSCF8574_edited.7d24e19b.jpg"
+  },
+  {
+    "revision": "0a45185367a1078b8c0c1e1b1226010e",
+    "url": "/static/media/DSCF8602_edited.0a451853.jpg"
+  },
+  {
+    "revision": "0631e90f7c090b0bcb8f144185a7a9ed",
+    "url": "/static/media/truck.0631e90f.svg"
+  }
+]);
